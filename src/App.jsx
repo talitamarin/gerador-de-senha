808c9c38700a2d635aa7e24eaef5da3d9f128fb3
talitamarin <128840773+talitamarin@users.noreply.github.com>
2023-06-26 14:07:32 -0300

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useTransition } from 'react';
 
 function App() {
   const [password, setPassword] = useState('');
   const [copyText, setCopyText] = useState('Copiar');
+  const [passwordSize, setPasswordSize] = useState(12);
 
   const generatePassword = () => {
-    const length = 10;
     const charset =
       'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789<>:?,.~()_1234567890!*#@$%';
 
     let result = '';
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const randomIndex = Math.floor(Math.random() * charset.length);
       result += charset[randomIndex];
     }
@@ -24,10 +24,22 @@ function App() {
   }
 
   return (
-    <div>
+    <div className="app">
+      <h1>Gerador de senhas</h1>
+      <div>
+        <label htmlFor="passwordSize">Tamanho:</label>
+        <input
+          type="number"
+          id="passwordSize"
+          min={1}
+          value={passwordSize}
+          onChange={(ev) => setPasswordSize(ev.target.value)}
+        />
+      </div>
+      <hr />
       <button onClick={generatePassword}>Gerar Senha</button>
       <button onClick={copytoClipboard}>{copyText}</button>
-      <p>{password}</p>
+      <div>{password}</div>
     </div>
   );
 }
